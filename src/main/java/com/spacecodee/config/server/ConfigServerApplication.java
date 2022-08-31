8d4ec7da38ebd0e91ab7f1a8decf3a1cb22ecfb3
spacecodee <spacecodee@gmail.com>
2022-08-31 18:24:53 -0500

    aDD>

--- src/main/java/com/spacecodee/config/server/ConfigServerApplication.java
+++ src/main/java/com/spacecodee/config/server/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.spacecodee.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
